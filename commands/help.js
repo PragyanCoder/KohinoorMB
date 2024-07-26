0e0390c2df7ae062679c75022e22021951095998
CoderXPragyan <168406488+PragyanCoder@users.noreply.github.com>
2024-07-27 00:27:22 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -38,7 +38,7 @@ Your ultimate music companion on Discord. Here are the available commands:
         .setTitle(`${botName} Help`)
         .setThumbnail(client.user.displayAvatarURL()) 
         .setDescription(helpDescription)
-        .setFooter({ text: `Prime Music v1.0`, iconURL: client.user.displayAvatarURL() }) 
+        .setFooter({ text: `Kohinoor Music v143.143`, iconURL: client.user.displayAvatarURL() }) 
       
 
       return interaction.reply({ embeds: [embed] });
